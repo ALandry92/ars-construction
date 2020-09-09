74cf01d587a12a74fa834993b6d20adae4df20f0
brandon-twitty <brandon.twitty@codestlouis.com>
2020-09-09 18:08:41 -0500

    fixed file path

---
+++ container/public_html/includes/header.php
@@ -0,0 +1,107 @@
+            <div class="top-bar bg-primary">
+                <div class="container">
+                    <div class="row">
+                        <div class="clearfix">
+                        	<div class="wt-topbar-left">
+                           	    <ul class="list-unstyled e-p-bx pull-left">
+                                    <li><i class="fa fa-envelope"></i>ARSOffice@advancedroofsys.com</li>
+                                    <li><i class="fa fa-phone"></i>(844) 445-ROOF</li>
+                                </ul>
+                            </div>
+                            
+                        	<!--<div class="wt-topbar-right">
+                        		<ul class="list-unstyled e-p-bx pull-right">
+                                    <li><a href="#" data-toggle="modal" data-target="#Login-form"><i class="fa fa-user"></i>Login</a></li>
+                                    <li><a href="#" data-toggle="modal" data-target="#Register-form"><i class="fa fa-sign-in"></i>Register</a></li>
+                                </ul>
+                            </div>-->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Search Link -->
+
+            <div class="main-bar header-middle bg-white">
+                <div class="container">
+                    <div class="logo-header">
+                        <a href="index.html">
+                            <img src="images/ARS LOGO-header.png"  alt="ARS Construction" />
+                        </a>
+                    </div>
+                    <div class="header-info">
+                        <ul>
+                        	<li>
+                                <div>
+                                    <div class="icon-sm">
+                                        <span class="icon-cell  text-primary"><i class="flaticon-placeholder"></i></span>
+                                    </div>
+                                    <div class="icon-content">
+                                        <strong>Our Location </strong>
+                                        <span>16943 Manchester Rd,<br> Wildwood, MO 63040</span>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    <div class="icon-sm">
+                                        <span class="icon-cell  text-primary"><i class="flaticon-smartphone"></i></span>
+                                    </div>
+                                    <div class="icon-content">
+                                        <strong>Phone Number</strong>
+                                        <span>(844) 455-ROOF</span>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="btn-col-last">
+
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="sticky-header main-bar-wraper">
+                <div class="main-bar header-botton nav-bg-secondry">
+                    <div class="container">
+                        <!-- NAV Toggle Button -->
+                        <button data-target=".header-nav" data-toggle="collapse" type="button" class="navbar-toggle collapsed">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <!-- ETRA Nav -->
+                        
+                        <!-- MAIN Nav -->
+                        <div class="header-nav navbar-collapse collapse ">
+                            <ul class=" nav navbar-nav">
+                                <li class="active">
+                                    <a href="index.html">Home</a>
+                                </li>
+                                <li>
+                                    <a href="gallery.html">Gallery</a>
+                                </li>
+                                <li>
+                                    <a href="residential.html">Residential</a>
+                                </li>
+                                <li>
+                                    <a href="commercial.html">Commercial</a>
+                                </li>
+                                <li class="active">
+                                    <a href="javascript:;">Home Improvement<i class="fa fa-chevron-down"></i></a>
+                                    <ul class="sub-menu">
+                                        <li><a href="exterior.html">Exterior</a></li>
+                                        <li><a href="interior.html">Interior</a></li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="about.html">About Us</a>
+                                </li>
+                                <li>
+                                    <a href="contact-us.html">Contact Us</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
